4f878aba54bf7753a5a07ec4c1b54468c3f0f154
lschpmn <luis@lmc-productions.com>
2016-03-24 20:15:46 -0500

    Added endpoint to send emails

--- server/index.js
+++ server/index.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const restify = /**@type {restify}*/ require('restify');
+const nodemailer = require('nodemailer');
+const server = restify.createServer();
+const PORT = 5001;
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+    user: process.env.user,
+    pass: process.env.pass
+  }
+});
+
+server.use(restify.CORS());
+server.use(restify.bodyParser());
+
+server.post('/api/email', (req, res) => {
+  console.log(req.body);
+
+  const email = {
+    to: process.env.to,
+    subject: 'Email from Personal Site',
+    text: `Message from Personal Site:\n\n\n ${req.body.email}`
+  };
+
+  transporter.sendMail(email, (err, info) => {
+    if(err) {
+      console.error('Error sending email');
+      console.error(err.stack);
+      return res.send(500);
+    }
+
+    console.log('Successfully sent Email');
+    res.send(200);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
