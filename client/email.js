826ae620062223433625cce2f62010c1b74d91da
lschpmn <luis@lmc-productions.com>
2016-03-24 22:08:49 -0500

    Client disables button when email is sent

--- client/email.js
+++ client/email.js
@@ -11,14 +11,13 @@ const emailVue = new vue({
   },
   methods: {
     sendEmail: function sendEmail() {
-      if(emailVue.emailText && emailVue.emailText !== 'Message Required') {
-        console.log(emailVue.emailText);
-        axios.post('/api/email', {email: emailVue.emailText})
-          .then(res => console.log(res.data))
-          .catch(err => console.log(err.stack));
-      } else {
-        emailVue.emailText = 'Message Required';
-      }
+      if(!emailVue.emailText || emailVue.emailText === 'Message Required') return emailVue.emailText = 'Message Required';
+      if(localStorage.getItem('sentEmail')) return;
+      localStorage.setItem('sentEmail', true);
+  
+      axios.post('/api/email', {email: emailVue.emailText})
+        .then(res => emailVue.submitText = 'Thank You!')
+        .catch(err => console.log(err.stack));
     }
   }
 });
